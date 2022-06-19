a209060e45ea485fb007706223ad7d2a28e2d923
Francis Cho <fhcho1@yahoo.com>
2022-06-19 10:08:22 -0700

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
